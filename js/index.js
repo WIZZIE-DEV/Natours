1b4e0336607cc38c608b8cb7daba972a0d64a4cc
wisdom ekeh <wisdom.dev@outlook.com>
2021-01-27 23:50:01 +0100

    Half-way done
    Half way done finished header,composition,feature box and card sections

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector(".btn");
+const btw = document.querySelector(".btn--white");
+const btnT = document.querySelector(".btn-text");
+const btnC = document.querySelector(".btn-card");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+btnT.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+
+btnC.addEventListener("click", (e) => {
+  e.preventDefault();
+});
